3483f5d8b247d45e4503f8d3c3f8e0b0c8c24561
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2022-12-22 23:14:48 +0100

    test(uncertain_inputs): introduce test for data_acquisition's uncertain_inputs module

---
+++ tests/test_uncertain_inputs.py
@@ -0,0 +1,37 @@
+import pytest
+
+from ilp_nn_robustness_verification.data_acquisition.uncertain_inputs import (
+    UncertainInputs,
+)
+from ilp_nn_robustness_verification.data_types import Intervals, UncertainArray
+
+
+@pytest.fixture(scope="session")
+def uncertain_inputs() -> UncertainInputs:
+    return UncertainInputs()
+
+
+def test_default_init_uncertain_inputs(uncertain_inputs: UncertainInputs) -> None:
+    assert uncertain_inputs
+
+
+def test_default_init_uncertain_inputs_has_values(
+    uncertain_inputs: UncertainInputs,
+) -> None:
+    assert hasattr(uncertain_inputs, "uncertain_values")
+
+
+def test_default_init_uncertain_inputs_values_are_uncertain_array(
+    uncertain_inputs: UncertainInputs,
+) -> None:
+    assert isinstance(uncertain_inputs.uncertain_values, UncertainArray)
+
+
+def test_default_init_uncertain_inputs_has_intervals(
+    uncertain_inputs: UncertainInputs,
+) -> None:
+    assert hasattr(uncertain_inputs, "intervals")
+
+
+def test_default_init_intervals_are_tuple(uncertain_inputs: UncertainInputs) -> None:
+    assert isinstance(uncertain_inputs.intervals, tuple)
