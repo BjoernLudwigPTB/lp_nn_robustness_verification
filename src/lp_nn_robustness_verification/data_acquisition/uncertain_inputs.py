cb9259e1541ef6e6d572533d7456b36bb1efa60a
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2022-12-27 17:18:16 -0500

    refactor(uncertain_inputs): introduce meaningful assert messages

--- src/lp_nn_robustness_verification/data_acquisition/uncertain_inputs.py
+++ src/lp_nn_robustness_verification/data_acquisition/uncertain_inputs.py
@@ -37,12 +37,25 @@ class UncertainInputs:
             len(self.uncertain_values.values.shape)
             == len(self.uncertain_values.uncertainties.shape)
             == 1
+        ), (
+            f"Either one or both of the values and the uncertainties are not given "
+            f"as a vector but the values are of shape "
+            f"{self.uncertain_values.values.shape} and the uncertainties of "
+            f"shape {self.uncertain_values.uncertainties.shape}"
         )
         assert len(self.uncertain_values.values) == len(
             self.uncertain_values.uncertainties
+        ), (
+            f"Somehow values and associated uncertainties are not of the same size "
+            f"but the values are of length {len(self.uncertain_values.values)} and "
+            f"the uncertainties of length {len(self.uncertain_values.uncertainties)}"
         )
         self.intervals = self.build_intervals(self.uncertain_values)
-        assert len(self.intervals) == len(self.uncertain_values.values)
+        assert len(self.intervals) == len(self.uncertain_values.values), (
+            f"Somehow there were not as many intervals calculated as there are values, "
+            f"but there are {len(self.intervals)} intervals and each "
+            f"{len(self.uncertain_values.values)} values and uncertainties"
+        )
 
     @staticmethod
     def build_intervals(uncertain_values: UncertainArray) -> Intervals:
