d4cf1f8038bf5fcd3ce112f57b7faf37d6336cc5
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2022-12-27 17:28:55 -0500

    feat(optimize): introduce creation of deeper network parameters according to pytorch implementation

--- src/lp_nn_robustness_verification/optimize.py
+++ src/lp_nn_robustness_verification/optimize.py
@@ -14,25 +14,30 @@ At the moment it is only possible to call it via:
 We might add command line parameters at a later time. For now please edit the main
 function at the very bottom of this file to change inputs.
 """
-import numpy as np
 from zema_emc_annotated.dataset import provide_zema_samples  # type: ignore[import]
 
+from lp_nn_robustness_verification.data_acquisition.activation_functions import Sigmoid
+from lp_nn_robustness_verification.data_acquisition.generate_nn_params import (
+    construct_partition,
+    generate_weights_and_biases,
+)
 from lp_nn_robustness_verification.data_acquisition.uncertain_inputs import (
     UncertainInputs,
 )
-from lp_nn_robustness_verification.data_types import NNParams, UncertainArray
+from lp_nn_robustness_verification.data_types import UncertainArray
 from lp_nn_robustness_verification.linear_program import RobustnessVerification
 from lp_nn_robustness_verification.pre_processing import LinearInclusion
 
 if __name__ == "__main__":
     zema_data = provide_zema_samples()
+    nn_params = generate_weights_and_biases(
+        len(zema_data.values[0]), construct_partition(len(zema_data.values[0]))
+    )
     for (values, uncertainties) in zip(zema_data.values, zema_data.uncertainties):
         linear_inclusion = LinearInclusion(
             uncertain_inputs=UncertainInputs(UncertainArray(values, uncertainties)),
-            nn_params=NNParams(
-                np.zeros_like(values)[np.newaxis, ...],
-                np.eye(len(uncertainties))[np.newaxis, ...],
-            ),
+            activation=Sigmoid,
+            nn_params=nn_params,
         )
         optimization_model = RobustnessVerification(linear_inclusion)
         print(optimization_model.solve())
