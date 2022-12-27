7e734768905c58a42d7a9b0a84a44b041053bdce
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2022-12-26 20:49:10 -0500

    refactor(data_types): introduce some more types and refinements

--- src/ilp_nn_robustness_verification/data_types.py
+++ src/ilp_nn_robustness_verification/data_types.py
@@ -4,8 +4,11 @@ __all__ = [
     "ActivationFunc",
     "Intervals",
     "IntervalCollection",
+    "LayerIdx",
+    "NeuronIdx",
     "NNParams",
     "RealMatrix",
+    "RealVars",
     "RealVector",
     "RealScalarFunction",
     "UncertainArray",
@@ -13,26 +16,35 @@ __all__ = [
     "VectorOfRealVectors",
 ]
 
+from dataclasses import dataclass
 from typing import Callable, cast, Iterator, NamedTuple, TypeAlias
 
 import numpy as np
 from interval import interval
 from numpy._typing import NDArray
 
-RealScalarFunction: TypeAlias = Callable[[float], float]
-"""A real-valued function with one real argument"""
 RealMatrix: TypeAlias = NDArray[np.float64]
 """A real matrix represented by a :class:`np.ndarray <numpy.ndarray>`"""
-RealVector = NDArray[np.float64]
+RealVector: TypeAlias = NDArray[np.float64]
 """A real vector represented by a :class:`np.ndarray <numpy.ndarray>`"""
 VectorOfRealVectors: TypeAlias = NDArray[np.float64]
 """A vector of real vectors represented by a :class:`np.ndarray <numpy.ndarray>`"""
 VectorOfRealMatrices: TypeAlias = NDArray[np.float64]
 """A vector of real matrices represented by a :class:`np.ndarray <numpy.ndarray>`"""
+RealScalarFunction: TypeAlias = Callable[
+    [np.float64 | RealVector], np.float64 | RealVector
+]
+"""A real-valued function with one real argument"""
 Intervals: TypeAlias = tuple[interval, ...]
 """A tuple of intervals on the real number line each enabled for interval arithmetics"""
 IntervalCollection: TypeAlias = tuple[Intervals, ...]
 """Tuple of tuples of intervals on the real number line interval arithmetics enabled"""
+LayerIdx: TypeAlias = int
+"""Index of a layer of a neural network"""
+NeuronIdx: TypeAlias = int
+"""Index of a neuron in a layer of a neural network"""
+RealVars: TypeAlias = dict[tuple[LayerIdx, NeuronIdx], float]
+"""The real variables in the linear optimization problem"""
 
 
 class UncertainArray(NamedTuple):
@@ -47,13 +59,14 @@ class UncertainArray(NamedTuple):
 class ActivationFunc(NamedTuple):
     """A representation of a real-valued, scalar function with its first derivative"""
 
-    func: RealScalarFunction = lambda x: x
+    func: RealScalarFunction = lambda x: np.full(1, x)
     """the function itself"""
-    deriv: RealScalarFunction = lambda x: 1
+    deriv: RealScalarFunction = lambda x: np.ones(1)
     """the function's derivative"""
 
 
-class NNParams(NamedTuple):
+@dataclass
+class NNParams:
     """A representation of a neural network's parameters"""
 
     biases: VectorOfRealVectors = np.array([[0.0, 0.0]])
@@ -61,7 +74,19 @@ class NNParams(NamedTuple):
     weights: VectorOfRealMatrices = np.array([[[1.0, 0.0], [0.0, 1.0]]])
     """The weights matrices of the neural network"""
 
-    def __iter__(  # type: ignore[override]
+    def __init__(
+        self,
+        biases: VectorOfRealVectors = np.array([[0.0, 0.0]]),
+        weights: VectorOfRealMatrices = np.array([[[1.0, 0.0], [0.0, 1.0]]]),
+    ):
+        assert len(biases) == len(weights)
+        for bias_vector, weight_matrix in zip(biases, weights):
+            assert len(weight_matrix.shape) == 2
+            assert len(bias_vector) == weight_matrix.shape[0]
+        self.biases = biases
+        self.weights = weights
+
+    def __iter__(
         self,
     ) -> Iterator[tuple[RealVector, RealMatrix]]:
         """Return an iterator over the biases and weights
