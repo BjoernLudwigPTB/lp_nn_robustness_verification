c4d2fd246d3c0fd011f083d12cabdb7a6e0f3388
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2023-01-07 11:48:32 -0500

    feat(generate_nn_params): introduce seeding into weight and bias generation

--- src/lp_nn_robustness_verification/data_acquisition/generate_nn_params.py
+++ src/lp_nn_robustness_verification/data_acquisition/generate_nn_params.py
@@ -16,26 +16,36 @@ def construct_partition(in_features: int) -> list[int]:
     return list(sorted(partition, reverse=True))
 
 
-def generate_weight_matrix(in_features: int, out_features: int) -> NDArray[np.float64]:
+def generate_weight_matrix(
+    in_features: int, out_features: int, seed: int | None = None
+) -> NDArray[np.float64]:
     """Initialize a weight matrix in accordance with Kaiming initialization scheme"""
     bound = 1 / math.sqrt(in_features)
-    return np.random.default_rng().uniform(-bound, bound, (out_features, in_features))
+    return np.random.default_rng(seed).uniform(
+        -bound, bound, (out_features, in_features)
+    )
 
 
-def generate_bias_vector(in_features: int, out_features: int) -> NDArray[np.float64]:
+def generate_bias_vector(
+    in_features: int, out_features: int, seed: int | None = None
+) -> NDArray[np.float64]:
     """Initialize a bias vector in accordance with pytorch initialization scheme"""
     bound = 1 / math.sqrt(in_features)
-    return np.random.default_rng().uniform(-bound, bound, out_features)
+    return np.random.default_rng(seed).uniform(-bound, bound, out_features)
 
 
-def generate_weights_and_biases(in_features: int, out_features: list[int]) -> NNParams:
+def generate_weights_and_biases(
+    in_features: int, out_features: list[int], seed: int | None = None
+) -> NNParams:
     """Initialize weight matrices and bias vectors according to Kaiming scheme"""
-    weight_matrices = [generate_weight_matrix(in_features, out_features[0])]
-    bias_vectors = [generate_bias_vector(in_features, out_features[0])]
+    weight_matrices = [generate_weight_matrix(in_features, out_features[0], seed)]
+    bias_vectors = [generate_bias_vector(in_features, out_features[0], seed)]
     if len(out_features) >= 2:
         for n_in_features, n_out_features in zip(out_features, out_features[1:]):
             weight_matrices.append(
-                generate_weight_matrix(n_in_features, n_out_features)
+                generate_weight_matrix(n_in_features, n_out_features, seed)
             )
-            bias_vectors.append(generate_bias_vector(n_in_features, n_out_features))
+            bias_vectors.append(
+                generate_bias_vector(n_in_features, n_out_features, seed)
+            )
     return NNParams(tuple(bias_vectors), tuple(weight_matrices))
