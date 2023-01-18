01f609be92cda7b889f7541f432d53b4cf6baeb0
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2023-01-18 17:39:44 +0100

    refactor(examples): reduce instances to what we know (and hope) is solvable

--- src/lp_nn_robustness_verification/examples/find_seeds_and_samples.py
+++ src/lp_nn_robustness_verification/examples/find_seeds_and_samples.py
@@ -36,7 +36,7 @@ def find_seeds_and_samples(task_id: int, proc_id: int) -> None:
         expected to lie between 0 and 27 each included
     """
     valid_seeds: dict[ScalerAndLayers, IndexAndSeed] = {}
-    size_scalers: list[int] = [1, 10, 100, 1000]
+    size_scalers: list[int] = [1, 10]
     depths: list[int] = [1, 3, 5, 8]
     for size_scaler in size_scalers:
         zema_data = ZeMASamples(100, size_scaler, True)
