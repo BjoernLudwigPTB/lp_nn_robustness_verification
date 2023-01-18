baf1c1d93817627ff3b0d693ce4fdc32884a2a08
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2023-01-18 17:37:47 +0100

    refactor(examples): adapt find_seeds_and_samples to new data_types location

--- src/lp_nn_robustness_verification/examples/find_seeds_and_samples.py
+++ src/lp_nn_robustness_verification/examples/find_seeds_and_samples.py
@@ -15,9 +15,11 @@ from lp_nn_robustness_verification.data_acquisition.uncertain_inputs import (
     UncertainInputs,
 )
 from lp_nn_robustness_verification.data_types import (
-    IndexAndSeed,
     UncertainArray,
-    ValidCombinationForZeMA,
+)
+from lp_nn_robustness_verification.examples.data_types import (
+    IndexAndSeed,
+    ScalerAndLayers,
 )
 from lp_nn_robustness_verification.linear_program import RobustnessVerification
 from lp_nn_robustness_verification.pre_processing import LinearInclusion
@@ -33,25 +35,20 @@ def find_seeds_and_samples(task_id: int, proc_id: int) -> None:
     proc_id : int
         expected to lie between 0 and 27 each included
     """
-    valid_seeds: dict[ValidCombinationForZeMA, IndexAndSeed] = {}
+    valid_seeds: dict[ScalerAndLayers, IndexAndSeed] = {}
     size_scalers: list[int] = [1, 10, 100, 1000]
     depths: list[int] = [1, 3, 5, 8]
     for size_scaler in size_scalers:
         zema_data = ZeMASamples(100, size_scaler, True)
         for depth in depths:
-            print(
-                f"Trying to find seed for {ValidCombinationForZeMA(size_scaler, depth)}"
-            )
+            print(f"Trying to find seed for {ScalerAndLayers(size_scaler, depth)}")
             for idx_start in range(100):
                 uncertain_inputs = UncertainInputs(
                     UncertainArray(
                         zema_data.values[idx_start], zema_data.uncertainties[idx_start]
                     )
                 )
-                if (
-                    valid_seeds.get(ValidCombinationForZeMA(size_scaler, depth))
-                    is not None
-                ):
+                if valid_seeds.get(ScalerAndLayers(size_scaler, depth)) is not None:
                     print(f"valid seeds: {valid_seeds}")
                     with open(
                         f"{size_scaler}_{depth}.txt", "a", encoding="utf-8"
@@ -80,9 +77,9 @@ def find_seeds_and_samples(task_id: int, proc_id: int) -> None:
                     optimization.model.hideOutput()
                     optimization.solve()
                     if optimization.model.getSols():
-                        valid_seeds[
-                            ValidCombinationForZeMA(size_scaler, depth)
-                        ] = IndexAndSeed(idx_start, seed)
+                        valid_seeds[ScalerAndLayers(size_scaler, depth)] = IndexAndSeed(
+                            idx_start, seed
+                        )
                         break
 
 
