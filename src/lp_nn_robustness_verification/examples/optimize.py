c2d8ab51cb5c1fa1010d3302af8008fad442f521
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2023-01-19 00:40:33 +0100

    refactor(optimize): move optimize script to examples and introduce profiling

--- src/lp_nn_robustness_verification/examples/optimize.py
+++ src/lp_nn_robustness_verification/examples/optimize.py
@@ -14,6 +14,7 @@ At the moment it is only possible to call it via:
 We might add command line parameters at a later time. For now please edit the main
 function at the very bottom of this file to change inputs.
 """
+import yappi  # type: ignore[import]
 from zema_emc_annotated.dataset import ZeMASamples  # type: ignore[import]
 
 from lp_nn_robustness_verification.data_acquisition.activation_functions import Sigmoid
@@ -29,16 +30,33 @@ from lp_nn_robustness_verification.linear_program import RobustnessVerification
 from lp_nn_robustness_verification.pre_processing import LinearInclusion
 
 if __name__ == "__main__":
-    zema_data = ZeMASamples(100, size_scaler=1, normalize=True)
+    SAMPLES_PER_SENSOR = 10
+    DEPTH = 1
+    zema_data = ZeMASamples(size_scaler=SAMPLES_PER_SENSOR, normalize=True)
     nn_params = generate_weights_and_biases(
         len(zema_data.values[0]),
-        construct_out_features_counts(len(zema_data.values[0])),
+        construct_out_features_counts(len(zema_data.values[0]), depth=DEPTH),
+        seed=0,
     )
     for (values, uncertainties) in zip(zema_data.values, zema_data.uncertainties):
+        yappi.start()
         linear_inclusion = LinearInclusion(
             uncertain_inputs=UncertainInputs(UncertainArray(values, uncertainties)),
             activation=Sigmoid,
             nn_params=nn_params,
         )
-        optimization_model = RobustnessVerification(linear_inclusion)
-        print(optimization_model.solve())
+        optimization = RobustnessVerification(linear_inclusion)
+        optimization.solve()
+        yappi.stop()
+        with open("timings.txt", "a", encoding="utf-8") as timings_file:
+            timings_file.write(
+                f"\n==================================================================="
+                f"===================\n"
+                f"Timings for {SAMPLES_PER_SENSOR * 11} inputs and {DEPTH} "
+                f"{'layers' if DEPTH > 1 else 'layer'}"
+                f"\n==================================================================="
+                f"===================\n"
+            )
+            yappi.get_func_stats().print_all(
+                out=timings_file, columns={0: ("name", 180), 3: ("ttot", 8)}
+            )
